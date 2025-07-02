a756d33a3b2d678e7f98b6f08e42f3541e032878
PranavSJ1402 <pranavsj02@gmail.com>
2025-07-02 13:24:25 +0530

    updated cofig file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio',
   images: {
-    unoptimized: true,
+    unoptimized: true, 
   },
 };
 
